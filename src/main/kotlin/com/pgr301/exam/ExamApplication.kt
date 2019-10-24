70f663535e191c69b73056fa0d92231462e543ea
anonymous <anonymous>
2019-10-24 12:19:37 +0200

    Setup logz.io

--- src/main/kotlin/com/pgr301/exam/ExamApplication.kt
+++ src/main/kotlin/com/pgr301/exam/ExamApplication.kt
@@ -2,9 +2,16 @@ package com.pgr301.exam
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import io.micrometer.core.instrument.simple.SimpleMeterRegistry
+import org.springframework.beans.factory.annotation.Autowired
+
+
 
 @SpringBootApplication
-class ExamApplication
+class ExamApplication {
+//    @Autowired
+//    private lateinit var meterRegistry: SimpleMeterRegistry
+}
 
 fun main(args: Array<String>) {
     runApplication<ExamApplication>(*args)
--- src/main/kotlin/com/pgr301/exam/controllers/DeviceController.kt
+++ src/main/kotlin/com/pgr301/exam/controllers/DeviceController.kt
@@ -5,6 +5,10 @@ import com.google.gson.Gson
 import com.pgr301.exam.entities.Device
 import com.pgr301.exam.entities.Measurement
 import com.pgr301.exam.repositories.DeviceRepository
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.slf4j.Marker
+import org.slf4j.MarkerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
@@ -15,6 +19,9 @@ import javax.validation.ConstraintViolationException
 @RequestMapping(path = ["/devices"], produces = [(MediaType.APPLICATION_JSON_VALUE)])
 @RestController
 class DeviceController {
+    val logger: Logger = LoggerFactory.getLogger(DeviceController::class.java)
+    val warnMarker: Marker = MarkerFactory.getMarker("WARN")
+    val infoMarker: Marker = MarkerFactory.getMarker("INFO")
 
     @Autowired
     private lateinit var crud: DeviceRepository
@@ -26,15 +33,23 @@ class DeviceController {
 
     @GetMapping(path = ["/{id}"])
     fun getOne(@PathVariable("id") pathId: String?): ResponseEntity<String> {
-
         val id: Long
         try {
             id = pathId!!.toLong()
         } catch (e: Exception) {
+            logger.error(warnMarker, "Tried to access nonexistent device")
             return ResponseEntity.status(404).build()
         }
 
-        return ResponseEntity.ok(Gson().toJson(crud.findById(id)))
+        val device = crud.findById(id)
+        return if (device.isPresent) {
+            logger.info(infoMarker, "Fetched device with id: $id")
+            ResponseEntity.ok(Gson().toJson(crud.findById(id)))
+        } else {
+            logger.error(warnMarker, "Tried to access nonexistent device")
+            ResponseEntity.status(404).body(Gson().toJson("{\"error\": \"Couldn't find Device with id: $id\"}"))
+        }
+
     }
 
     @PostMapping(consumes = [(MediaType.APPLICATION_JSON_VALUE)])
