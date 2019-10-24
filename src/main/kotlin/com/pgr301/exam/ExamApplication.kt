3b7b1e9d65858631704f3df11cdb8d8d10f9449c
anonymous <anonymous>
2019-10-24 13:51:28 +0200

    Fix correct heroku token and setup influxdb connection to local instance

--- src/main/kotlin/com/pgr301/exam/ExamApplication.kt
+++ src/main/kotlin/com/pgr301/exam/ExamApplication.kt
@@ -1,16 +1,16 @@
 package com.pgr301.exam
 
+import io.micrometer.core.instrument.MeterRegistry
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import io.micrometer.core.instrument.simple.SimpleMeterRegistry
 import org.springframework.beans.factory.annotation.Autowired
 
 
 
 @SpringBootApplication
 class ExamApplication {
-//    @Autowired
-//    private lateinit var meterRegistry: SimpleMeterRegistry
+    @Autowired
+    private lateinit var meterRegistry: MeterRegistry
 }
 
 fun main(args: Array<String>) {
